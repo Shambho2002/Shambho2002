77934d8485bf17bacd092c07ec55576839d03161
Shambho2002 <sharshambho@gmail.com>
2025-07-01 10:00:29 +0530

    Create InsuranceService.java

---
+++ InternshipProject-main/src/main/java/com/rutuja/InsuranceManagementSystem/service/InsuranceService.java
@@ -0,0 +1,34 @@
+package com.rutuja.InsuranceManagementSystem.service;
+
+import com.rutuja.InsuranceManagementSystem.domain.Insurance;
+import com.rutuja.InsuranceManagementSystem.repository.InsuranceRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InsuranceService {
+	@Autowired
+	private InsuranceRepository repo;
+
+	public List<Insurance> listAll()
+	{
+		return repo.findAll();
+	}
+
+	public void save(Insurance policy)
+	{
+		repo.save(policy);
+	}
+
+	public Insurance get(int policyId)
+	{
+		return repo.findById(policyId).get();
+	}
+	public void delete(int policyId)
+	{
+		repo.deleteById(policyId);
+	}
+}
