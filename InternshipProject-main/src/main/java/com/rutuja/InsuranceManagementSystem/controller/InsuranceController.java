133c45cddadb0f4fc32e1a276a80961a499dcbe9
Shambho2002 <sharshambho@gmail.com>
2025-07-01 09:59:17 +0530

    Create InsuranceController.java

---
+++ InternshipProject-main/src/main/java/com/rutuja/InsuranceManagementSystem/controller/InsuranceController.java
@@ -0,0 +1,90 @@
+package com.rutuja.InsuranceManagementSystem.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.rutuja.InsuranceManagementSystem.chainOfRes.Handler;
+import com.rutuja.InsuranceManagementSystem.domain.Insurance;
+import com.rutuja.InsuranceManagementSystem.domain.User;
+import com.rutuja.InsuranceManagementSystem.facade.GetApproval;
+import com.rutuja.InsuranceManagementSystem.service.InsuranceService;
+
+@Controller
+public class InsuranceController {
+	@Autowired
+	private InsuranceService service;
+
+	@GetMapping("/managePolicy")
+	public String viewHomePage(Model model)
+	{
+		List<Insurance> listOfInsurances = service.listAll();
+		model.addAttribute("listOfInsurance",listOfInsurances);
+		return "managePolicyHomePage";
+	}
+
+	@GetMapping("/new")
+	public String add(Model model)
+	{
+		model.addAttribute("insurance",new Insurance());
+		return "BuyNewPolicyPage";
+	}
+
+	@RequestMapping("/search")
+	public String search(Model model)
+	{
+		model.addAttribute("searchInsurance",new Insurance());
+		return "search";
+	}
+
+	@RequestMapping(value = "/findPolicy", method = RequestMethod.POST)
+	public String findPolicy(@ModelAttribute("searchInsurance") Insurance policy, Model model)
+	{
+		List<Insurance> allPolicies = service.listAll();
+		List<Insurance> resultPolicies = new ArrayList<>();
+		for (Insurance i :
+			allPolicies) {
+			if(i.getPolicyType().equals(policy.getPolicyType()))
+			{
+				resultPolicies.add(i);
+			}
+		}
+		model.addAttribute("searchResults", resultPolicies);
+		return "searchRes";
+	}
+
+	@RequestMapping(value = "/save",method = RequestMethod.POST)
+	public String saveInsurance(@ModelAttribute("insurance") Insurance policy)
+	{
+		Handler handler = new Handler(policy.getPremium());
+		GetApproval getApproval = new GetApproval(policy.getPremium(), User.getUserInstance().getUsername());
+		if (handler.handle() && getApproval.approve()) {
+			service.save(policy);
+			return "redirect:/managePolicy";
+		}
+		return "redirect:/managePolicy?error";
+	}
+
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditInsurancePage(@PathVariable(name = "id") int policyId) {
+		ModelAndView mav = new ModelAndView("BuyNewPolicyPage");
+		Insurance policy = service.get(policyId);
+		mav.addObject("insurance", policy);
+		return mav;
+
+	}
+	@RequestMapping("/delete/{id}")
+	public String deletePolicy(@PathVariable(name = "id") int policyId) {
+		service.delete(policyId);
+		return "redirect:/managePolicy";
+	}
+}
